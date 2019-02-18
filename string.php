d385f6e47c3914156a2a3a7394e0831434823c93
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-18 17:19:23 -0300

    Tratamento de strings

---
+++ string.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome =  "Rutemberg barbosa";
+
+$nome = strtolower($nome);
+echo "$nome <hr>";
+
+$nome = strtoupper($nome);
+echo "$nome <hr>";
+
+$nome = ucwords($nome);
+echo "$nome <hr>";
+
+$nome = ucfirst($nome);
+echo "$nome <hr>";
+
+ ?>
