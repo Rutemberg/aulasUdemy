63dd54e9d0068581e4f724461d4b0ef4643d3d76
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-18 17:22:00 -0300

    <hr>

--- string.php
+++ string.php
@@ -1,17 +1,19 @@
 <?php
 
-$nome =  "Rutemberg barbosa";
+$nome =  "rutemberg barbosa";
+
+
+$nome = strtoupper($nome);
+echo "$nome <hr>";
 
 $nome = strtolower($nome);
 echo "$nome <hr>";
 
-$nome = strtoupper($nome);
+$nome = ucfirst($nome);
 echo "$nome <hr>";
 
 $nome = ucwords($nome);
 echo "$nome <hr>";
 
-$nome = ucfirst($nome);
-echo "$nome <hr>";
 
  ?>
