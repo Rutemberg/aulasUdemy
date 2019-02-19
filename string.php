fdbd2c1278b694be7489268443918cf1b20fafa8
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-19 09:49:01 -0300

    Pesquisa em frase

--- string.php
+++ string.php
@@ -2,7 +2,6 @@
 
 $nome =  "rutemberg barbosa";
 
-
 $nome = strtoupper($nome);
 echo "$nome <hr>";
 
@@ -15,5 +14,25 @@ echo "$nome <hr>";
 $nome = ucwords($nome);
 echo "$nome <hr>";
 
+/////////////////////////////////////////////////
 
+$empresa = "Hcode";
+
+$empresa = str_replace("o", "0", $empresa);
+$empresa = str_replace("e", "3", $empresa);
+
+echo "$empresa <hr>";
+
+/////////////////////////////////////////////////
+
+$frase = "A repetição é a mãe da retenção";
+
+$palavra = "mãe";
+
+$q = strpos($frase, $palavra);
+$texto = substr($frase, 0, $q);
+//var_dump($texto);
+
+$texto2 = substr($frase,$q + strlen($palavra), strlen($frase));
+var_dump($texto2);
  ?>
