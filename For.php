6d9040cdf88a20b3d02cbcb6703aa01e9ac371b2
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-19 16:28:26 -0300

    Recebendo GET na mesma página e tratando dados Foreach

--- For.php
+++ For.php
@@ -1,26 +1,48 @@
 <?php
 
-echo "<select>";
-for ($i=date('Y'); $i >= date('Y')-100; $i--) {
+/*
+    echo "<select>";
+    for ($i=date('Y'); $i >= date('Y')-100; $i--) {
 
-echo '<option value="'.$i.'">'.$i.'</option>';
+    echo '<option value="'.$i.'">'.$i.'</option>';
 
-}
-
-echo "</select><hr>";
-
+    }
+    echo "</select><hr>";
+*/
 
 ///////////////////////////////////////////////////////
 
-$meses = array(
-      "janeiro", "Fevereiro", "março",
-      "abril", "maio", "junho",
-      "julho", "agosto", "setembro",
-      "outubro", "novembro", "dezembro",
-);
+/*
+  $meses = array(
+        "janeiro", "Fevereiro", "março",
+        "abril", "maio", "junho",
+        "julho", "agosto", "setembro",
+        "outubro", "novembro", "dezembro",
+  );
 
-foreach ($meses as $index => $mes) {
-  echo $index."<br>";
-  echo $mes."<br><br>";
-}
+  foreach ($meses as $index => $mes) {
+    echo $index."<br>";
+    echo $mes."<br><br>";
+  }
+  */
+
  ?>
+
+ <form>
+   <input type="text" name="nome">
+   <input type="date" name="datanascimento">
+   <input type="submit" value="ok">
+
+ </form>
+
+ <?php
+
+foreach ($_GET as $key => $value) {
+  echo "Campo: {$key}<br>";
+  echo "Valor: {$value}<hr>";
+
+}
+
+
+
+  ?>
