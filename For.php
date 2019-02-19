ce2bb40af172170c9ff3276ae80440956bfc4fa4
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-19 16:15:29 -0300

    Foreach

--- For.php
+++ For.php
@@ -7,6 +7,20 @@ echo '<option value="'.$i.'">'.$i.'</option>';
 
 }
 
-echo "</select>";
+echo "</select><hr>";
 
+
+///////////////////////////////////////////////////////
+
+$meses = array(
+      "janeiro", "Fevereiro", "março",
+      "abril", "maio", "junho",
+      "julho", "agosto", "setembro",
+      "outubro", "novembro", "dezembro",
+);
+
+foreach ($meses as $index => $mes) {
+  echo $index."<br>";
+  echo $mes."<br><br>";
+}
  ?>
