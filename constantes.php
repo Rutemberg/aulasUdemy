7ef70dce8c3431d075b30a4a77168eaf28755c72
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-20 12:21:42 -0300

    Constantes

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+define("SERVIDOR", "127.0.0.1");
+define("BANCO_DE_DADOS", [
+    "nome" => "db",
+    "Host" => "127.0.0.1",
+    "root" => " "
+]);
+
+echo SERVIDOR."<br>";
+print_r(BANCO_DE_DADOS);
+
+
+
+ ?>
