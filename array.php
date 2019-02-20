e4a6dc8ed8cbe3fac9cb5723b1e152ec8adfcfba
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-20 10:56:32 -0300

    array bi

--- array.php
+++ array.php
@@ -7,11 +7,27 @@ print_r($array);
 
 /////////////////////////////////////////////
 
+echo "<hr>";
+
 $arrayBi[0][0] = "Gm";
 $arrayBi[0][1] = "Camaro";
 $arrayBi[0][2] = "Uno";
 $arrayBi[0][3] = "Fiesta";
 
 echo end($arrayBi[0]);
+
+////////////////////////////////////////////
+
+echo "<hr>";
+
+$Pessoas = array();
+
+array_push($Pessoas, array(
+    'nome' => "Joao",
+    'idade' => 18
+));
+
+
+print_r($Pessoas[0]['nome']);
 
  ?>
