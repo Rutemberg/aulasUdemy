605d1191706e7736ca406648e6365bffb9c54247
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-20 10:50:53 -0300

    array

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$array = array("laranja", "uva", "Beterraba");
+
+print_r($array);
+
+/////////////////////////////////////////////
+
+$arrayBi[0][0] = "Gm";
+$arrayBi[0][1] = "Camaro";
+$arrayBi[0][2] = "Uno";
+$arrayBi[0][3] = "Fiesta";
+
+echo end($arrayBi[0]);
+
+ ?>
