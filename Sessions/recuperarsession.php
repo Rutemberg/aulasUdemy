e32478df66773ba79eea640d7ea347b74a99ab60
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-20 15:39:55 -0300

    Sessões

---
+++ Sessions/recuperarsession.php
@@ -0,0 +1,7 @@
+<?php
+session_id("8k94anhjqimttmn4ss98blhohh");
+require_once("config.php");
+
+var_dump($_SESSION);
+
+ ?>
---
+++ Sessions/sessionsver.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('config.php');
+
+echo $_SESSION['nome']."<br>";
+echo session_id();
+
+var_dump($_SESSION);
+ ?>
