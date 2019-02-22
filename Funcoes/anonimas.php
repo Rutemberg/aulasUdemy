b4fa3af8a0ea3e74e5e236f60f4b104274e54621
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-22 12:32:58 -0300

    $callback

--- Funcoes/anonimas.php
+++ Funcoes/anonimas.php
@@ -1,10 +1,23 @@
 <?php
 
 
+function test($callback)
+{
+    $callback();
+}
+
+test(function(){
+
+    echo "terminou";
+
+});
+
 $mostrar = function ($a)
 {
     echo $a;
 };
 
+echo "<br>";
 $mostrar("oi");
+
  ?>
