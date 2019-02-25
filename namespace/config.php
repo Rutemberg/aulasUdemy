79bc51529d88d626ee096ccfaa7d1ca5a5961d51
Rutemberg <rutembergbarbosa@gmail.com>
2019-02-25 17:15:31 -0300

    comentado

--- namespace/config.php
+++ namespace/config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($nome_classe){
 
-    var_dump($nome_classe);
+    //var_dump($nome_classe);
     $dir_class = "classe";
     $filename = $dir_class . DIRECTORY_SEPARATOR . $nome_classe . ".php";
 
