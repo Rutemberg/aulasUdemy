1198485bd82dfed80fe0df083e2208def95dba6e
Rutemberg <rutembergbarbosa@gmail.com>
2019-03-06 10:54:09 -0300

    ReCaptcha

---
+++ seguranca/ReCaptcha.php
@@ -0,0 +1,7 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<form class="" action="cadastrar.php" method="post">
+  <div class="g-recaptcha" data-sitekey="6LcZ3ZUUAAAAAN6CbfajpWOZhLNvy0QUe34SgZTu"></div>
+  <input type="email" name="email">
+  <button type="submit">enviar</button>
+</form>
